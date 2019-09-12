1108acec4fde9cce9652cc8e5ef3c0cdf3181203
Nadia Daruiz <nadiadaruiz@gmail.com>
2019-09-12 12:53:05 +0200

    Return a func reference

--- closure-db-api.js
+++ closure-db-api.js
@@ -1,1 +1,16 @@
-const init = 
+let init = () => {
+  let db = [];
+
+  let create = newElement => {
+    db.push(newElement);
+    return db.length;
+  };
+
+  return create();
+};
+
+let createInDb = init();
+
+console.log(createInDb[1]());
+console.log(createInDb(2));
+console.log(createInDb(3));
