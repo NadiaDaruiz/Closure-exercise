01451f2261bfc0068d9aebd705ac7c7f16fc9483
Nadia Daruiz <nadiadaruiz@gmail.com>
2019-09-13 14:17:01 +0200

    Add delete func

--- closure-db-api.js
+++ closure-db-api.js
@@ -3,29 +3,46 @@ const init = () => {
 
   const create = element => {
     db.push(element);
-    return db.length;
+    //return db.length;
+    return `New element added! The new length of the array is ${db.length}.`;
   };
 
   const read = () => {
-    let dbCopy = [...db];
-    return dbCopy;
+    return [...db];
   };
 
   const update = (index, newElement) => {
-    if (db[index]) return db.splice(index, 1, newElement);
-    else {
-      return "Error. That element doesn't exists";
+    if (index >= 0 && index <= db.length - 1) {
+      return db.splice(index, 1, newElement);
+    } else {
+      return create(newElement);
+    }
+  };
+
+  const remove = removeFromIndex => {
+    if (removeFromIndex >= 0 && removeFromIndex <= db.length - 1) {
+      return db.splice(removeFromIndex, 1);
+    } else {
+      return "Error. The element doesn't exists.";
     }
   };
 
   let api = [];
   console.log(api);
+
   api.push(create);
   api.push(read);
   api.push(update);
+  api.push(remove);
   console.log(api);
+
+  //const apiFunctions = [create(), read(), update(), remove()];
+
   return api;
 };
+
+//const apiUpdate = apiFunctions[2];
+//const apiRemove = apiFunctions[3];
 
 const createInDb = init();
 
@@ -43,4 +60,5 @@ console.log(createInDb[0]("chair"));
 //console.log(createInDb[1]());
 console.log(createInDb[2]([5], "turtle"));
 console.log(createInDb[1]());
-console.log(createInDb[2]([8], "turtle"));
+console.log(createInDb[2](6, "hamster"));
+console.log(createInDb[1]());
