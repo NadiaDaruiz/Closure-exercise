1b5e1299a5ce86e739a6cc0d38f6942f92120486
Nadia Daruiz <nadiadaruiz@gmail.com>
2019-09-13 13:11:44 +0200

    Add update func

--- closure-db-api.js
+++ closure-db-api.js
@@ -1,6 +1,5 @@
 const init = () => {
   let db = [];
-  let api = [];
 
   const create = element => {
     db.push(element);
@@ -12,12 +11,19 @@ const init = () => {
     return dbCopy;
   };
 
-  //const update = (index, newElement) => {};
+  const update = (index, newElement) => {
+    if (db[index]) return db.splice(index, 1, newElement);
+    else {
+      return "Error. That element doesn't exists";
+    }
+  };
 
+  let api = [];
+  console.log(api);
   api.push(create);
   api.push(read);
-  //api.push(update);
-
+  api.push(update);
+  console.log(api);
   return api;
 };
 
@@ -33,3 +39,8 @@ console.log(createInDb[1]());
 createInDb[0]("lizard");
 console.log(createInDb[1]());
 createInDb[1]();
+console.log(createInDb[0]("chair"));
+//console.log(createInDb[1]());
+console.log(createInDb[2]([5], "turtle"));
+console.log(createInDb[1]());
+console.log(createInDb[2]([8], "turtle"));
